69bb4acebe28ac7496612be68ede2f1f056e8ce6
sinisi <sebastian@momentos.dk>
2014-05-09 19:36:31 +0200

    First version whit out string manipulation

---
+++ Shipplotter.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class Shipplotter {
+
+  private final String USER_AGENT = "Mozilla/5.0";
+
+  public static void main(String[] args) throws Exception {
+
+    Shipplotter http = new  Shipplotter();
+
+    System.out.println("\nTesting 2 - Send Http POST request");
+    http.sendPost();
+
+  }
+
+  // HTTP POST request
+  private void sendPost() throws Exception {
+
+    String url = "http://www.coaa.co.uk/shipinfo.php";
+    URL obj = new URL(url);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+    //add reuqest header
+    con.setRequestMethod("POST");
+    con.setRequestProperty("User-Agent", USER_AGENT);
+    con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+    String urlParameters = "Lines=0&Reg=57470&Extracode=48&Uponly=0&Mouset=86&LatN=+56.518685&LatS=+55.553905&LonE=++14.464614&LonW=++10.767836&Fwd=0&Ver=12.4.8&Tt=1399648978&Desig=219001684";
+
+    // Send post request
+    con.setDoOutput(true);
+    DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+    wr.writeBytes(urlParameters);
+    wr.flush();
+    wr.close();
+
+    int responseCode = con.getResponseCode();
+    System.out.println("\nSending 'POST' request to URL : " + url);
+    System.out.println("Post parameters : " + urlParameters);
+    System.out.println("Response Code : " + responseCode);
+
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(con.getInputStream()));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+
+    while ((inputLine = in.readLine()) != null) {
+      System.out.println(inputLine);
+    }
+    in.close();
+
+    //print result
+    System.out.println(response.toString());
+
+  }
+
+}
