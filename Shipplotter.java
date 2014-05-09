6f39423b84c5ce2d02889cc34d461f7fb16161b3
sinisi <sebastian@momentos.dk>
2014-05-09 22:06:53 +0200

    string manipulation in to comma ceparated csv

--- Shipplotter.java
+++ Shipplotter.java
@@ -46,11 +46,26 @@ public class Shipplotter {
 
     BufferedReader in = new BufferedReader(
             new InputStreamReader(con.getInputStream()));
-    String inputLine;
+    String ln;
     StringBuffer response = new StringBuffer();
+    while ((ln = in.readLine()) != null) {
+      if ((ln.charAt(26) == ' '))
+        {
+          ln = (ln.substring(0, 23) + " 0" + ln.substring(24));
+        }
+      System.out.println(ln);
 
-    while ((inputLine = in.readLine()) != null) {
-      System.out.println(inputLine);
+      ln = (ln.substring(0, 9) + "," + ln.substring(10, 20) + "," + ln.substring(21, 23) + "," + ln.substring(24, 27) + "," +
+            ln.substring(29, 38) + "," + ln.substring(41, 50) + "," + ln.substring(51, 56) + "," + ln.substring(57,62) + "," +
+            ln.substring(63, 66) + "," + ln.substring(67, 71) + "," + ln.substring(72, 75) + "," + ln.substring(76, 78) + "," + 
+            ln.substring(79, 99) + "," + ln.substring(100, 107) + "," + ln.substring(108, 128) + "," + ln.substring(129,134) + "," + 
+            ln.substring(135, 140) + "," + ln.substring(141, 142) + "," + ln.substring(143, 150) + "," + ln.substring(151,154) + "," + 
+            ln.substring(155, 157) + "," + ln.substring(158, 159));
+
+      //String newInputLine = inputLine.replace("  ", "");
+      //newInputLine = inputLine.replace(" ", ",");
+      System.out.println(ln);
+
     }
     in.close();
 
