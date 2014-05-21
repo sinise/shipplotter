b2595415e3ab14ca532b00263ddf2766db178e3b
sinisi <sebastian@momentos.dk>
2014-05-21 16:02:18 +0200

    added alfa of sql to csv

---
+++ SqlToLog.java
@@ -0,0 +1,56 @@
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Retrieve {
+    
+    public static void main(String[] args) {
+
+        Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "testuser";
+        String password = "test623";
+
+        try {
+            
+            con = DriverManager.getConnection(url, user, password);
+            pst = con.prepareStatement("SELECT * FROM Authors");
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                System.out.print(rs.getInt(1));
+                System.out.print(": ");
+                System.out.println(rs.getString(2));
+            }
+
+        } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(Retrieve.class.getName());
+                lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pst != null) {
+                    pst.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(Retrieve.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
+}
