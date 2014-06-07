8e62060c90813e5dc879c87cec90dc09171a215e
sinisi <sebastian@momentos.dk>
2014-06-07 16:12:03 +0200

    fasefsadfsad

--- MarineTraficCrawler.java
+++ MarineTraficCrawler.java
@@ -2,65 +2,78 @@ import java.net.*;
 import java.io.*;
 import java.awt.Toolkit;
 import java.util.*;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.SourceDataLine;
 //import java.sql.*;
 import java.text.SimpleDateFormat;
 
 public class MarineTraficCrawler {
   public static void main(String[] args) throws Exception {
 
+    ArrayList<String> returnedList = fetchMarineTrafic(1);
+
+    for (int i = 0; i < returnedList.size(); i++) {
+     System.out.println(returnedList.get(i));
+    }
+
+
+  }
+  /**
+   * fetch content from Marinetrafic. either from a saved html file or from url
+   * @param choice 0 to fecch online 1 to fetch from file
+   */
+  public static ArrayList<String> fetchMarineTrafic(int choice){
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-
     ArrayList<String> list = new ArrayList<String>();
-//  BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+    BufferedReader in;
 
-    URL url;
-    URLConnection uc;
-
-    String urlString = "http://www.marinetraffic.com/dk/ais/index/positions/all/mmsi:230964000/shipname:FUTURA/per_page:3000/page:1";
-    String regEx = "<td><span>";
-    String regStartLine = "<tr><td><span>";
-//    System.out.println("Getting content for URl : " + urlString);
-    url = new URL(urlString);
-    uc = url.openConnection();
-    uc.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-    uc.connect();
-    uc.getInputStream();
-    BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
-    String ch;
-    String parsedData = "";
-
-    while ((ch = in.readLine()) != null) {
-      if (ch.contains(regEx)) {
-
-        int indexEnd = ch.lastIndexOf("</span>");
-        int  indexStart = ch.indexOf("<span>") + 6;
-        if (ch.contains(regStartLine)) {
-          list.add(parsedData);
-
-          Date d = sdf.parse(ch.substring(indexStart, indexEnd));
-          Calendar c = Calendar.getInstance();
-          c.setTime(d);
-          long timestamp = c.getTimeInMillis();
-          parsedData = parsedData.valueOf(timestamp);
-
-//          parsedData = ch;
-        }
-        else {
-          parsedData = parsedData + ("," + ch.substring(indexStart, indexEnd));
-//          parsedData = parsedData + ("," + ch);
-        }
-
+    try{
+      if (choice == 0) {
+        URL url;
+        URLConnection uc;
+        String urlString = "http://www.marinetraffic.com/dk/ais/index/positions/all/mmsi:230964000/shipname:FUTURA/per_page:3000/page:1";
+        url = new URL(urlString);
+        uc = url.openConnection();
+        uc.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+        uc.connect();
+        in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
       }
-//      System.out.print(parsedData);
+      else {
+          FileInputStream fstream = new FileInputStream("l575.htm");
+          DataInputStream fin = new DataInputStream(fstream);
+          in = new BufferedReader(new InputStreamReader(fin));
+      }
+      String ch;
+      String parsedData = "";
+      String regEx = "<td><span>";
+      String regStartLine = "<tr><td><span>";
+
+      while ((ch = in.readLine()) != null) {
+        if (ch.contains(regEx)) {
+
+          int indexEnd = ch.lastIndexOf("</span>");
+          int  indexStart = ch.indexOf("<span>") + 6;
+          if (ch.contains(regStartLine)) {
+            list.add(parsedData);
+
+            Date d = sdf.parse(ch.substring(indexStart, indexEnd));
+            Calendar c = Calendar.getInstance();
+            c.setTime(d);
+            long timestamp = c.getTimeInMillis();
+            parsedData = parsedData.valueOf(timestamp);
+
+          }
+          else {
+            parsedData = parsedData + ("," + ch.substring(indexStart, indexEnd));
+          }
+
+        }
+      }
+      System.out.printf("Fetched %d positions", list.size());
+      in.close();
+
     }
-    for (int i = 0; i < list.size(); i++) {
-      System.out.println(list.get(i));
+    catch (Exception e) {
+      System.err.println("Error: " + e.getMessage());
     }
-    System.out.printf("listen er %d lanf", list.size());
-}
+    return list;
+  }
 }
